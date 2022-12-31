400e8b6564a016d7d378b14b5a36ff1519125a77
willyfh <willyfitrahendria@gmail.com>
2022-12-31 09:49:26 +0900

    add shell script to train the model

---
+++ scripts/train_msvd.sh
@@ -0,0 +1,26 @@
+# Setup
+DATATYPE=msvd
+N_GPU=2
+N_THREAD=16
+
+DATA_PATH=../dataset/MSVD
+CKPT_ROOT=../ckpts
+INIT_MODEL_PATH=../weight/univl.pretrained.bin
+# Path to the features you extracted from CLIP4Clip
+FEATURES_PATH=../extracted_feats/msvd/MSVD_CLIP4Clip_features.pickle
+# Tuning Params
+LEARNING_RATE=(1e-5 3e-5 7e-5 1e-4 3e-4 7e-4)
+
+for lr in "${LEARNING_RATE[@]}"
+do
+  python -m torch.distributed.launch --nproc_per_node=${N_GPU} \
+  ../train.py --do_train --num_thread_reader=${N_THREAD} \
+  --epochs=50 --batch_size=128 --n_display=50 --gradient_accumulation_steps 1 \
+  --data_path ${DATA_PATH} --features_path ${FEATURES_PATH} \
+  --output_dir ${CKPT_ROOT}/${DATATYPE}_lr${lr}_gnn \
+  --bert_model bert-base-uncased --do_lower_case \
+  --lr ${lr} --max_words 48 --max_frames 20 --batch_size_val 16 \
+  --visual_num_hidden_layers 2 --decoder_num_hidden_layers 2 \
+  --datatype ${DATATYPE} --init_model ${INIT_MODEL_PATH} \
+  --d_model 512 --video_dim 512
+done
